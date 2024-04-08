6fa4e453ccbfb386ffba0d6cbbed43252dc73633
Aaron-Muuo <aaronmuuo60@gmail.com>
2024-04-08 08:24:25 +0000

    project 3

---
+++ project 3 - online store/javascript/script.js
@@ -0,0 +1,14 @@
+// no js code
+
+
+document.getElementById('login-button').addEventListener('click', (event)=>{
+
+    let email = document.getElementById('input-email').value;
+    let password = document.getElementById('input-password').value;
+
+    //get credentials from an API
+    //validate
+    //redirect to dashboard or show an error
+
+    
+})
