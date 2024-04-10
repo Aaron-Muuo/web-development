cd535662dce55974fba289743460bd27e8e19838
Aaron-Muuo <aaronmuuo60@gmail.com>
2024-04-10 13:19:55 +0000

    Added dashbord, login and cart

--- project 3 - online store/javascript/dashboard.js
+++ project 3 - online store/javascript/dashboard.js
@@ -1,1 +1,85 @@
-// no js code
+document.addEventListener('DOMContentLoaded', function() {
+
+    let userProfile = localStorage.getItem('userProfile');
+
+    // Check if userProfile exists in localStorage
+    if (userProfile) {
+
+        let user_data = JSON.parse(userProfile);
+
+        document.getElementById('txt-username').innerHTML = user_data.username;
+        document.getElementById('txt-email').innerHTML = user_data.email;
+    }
+
+    getProducts();
+    
+
+    document.getElementById('btn-logout').addEventListener('click', (event)=>{
+
+        // Prevent default form submission behavior
+        event.preventDefault();
+
+       // Remove item from localStorage
+        localStorage.removeItem('userProfile');
+
+        // Clear all items from localStorage
+        localStorage.clear();
+
+        document.location.href='login.html';
+    
+       
+    });
+
+    async function getProducts() {
+
+        let loading_section = document.getElementById('loading-section');
+
+        try {
+            const response = await fetch('https://fakestoreapi.com/products');
+            if (!response.ok) {
+                loading_section.style.backgroundColor = "red";
+                loading_section.style.display = "block";
+                loading_section.style.innerHTML = "Failed to fetch product data";
+                //throw new Error('Failed to fetch product data');
+            }else{
+
+                loading_section.style.display = "none";
+            }
+            const data = await response.json();
+            displayProducts(data);
+        } catch (error) {
+            loading_section.style.backgroundColor = "red";
+            loading_section.style.display = "block";
+            loading_section.style.innerHTML = "Failed to fetch product data";
+            console.error('Error fetching data:', error);
+        }
+     
+        // fetch('https://fakestoreapi.com/products')
+        // .then(response => response.json())
+        // .then(data => {
+        //     // Process the retrieved data
+        //     console.log(data);
+        // })
+        // .catch(error => {
+        //     console.error('Error fetching data:', error);
+        // });
+      }
+
+      function displayProducts(products) {
+        const productContainer = document.getElementById('product-container');
+        
+        products.forEach(product => {
+            const section = document.createElement('section');
+            section.classList.add('product');
+            section.innerHTML = `
+                <img src="${product.image}" width="100%" alt="${product.title}">
+                <h2>${product.title}</h2>
+                <p class="price">Kes ${product.price}</p>
+                <button class="add-to-cart">Add to Cart</button>
+            `;
+            productContainer.appendChild(section);
+        });
+    }
+    
+
+});
--- project 3 - online store/javascript/script.js
+++ project 3 - online store/javascript/script.js
@@ -3,12 +3,55 @@
 
 document.getElementById('login-button').addEventListener('click', (event)=>{
 
+    // Prevent default form submission behavior
+    event.preventDefault(); 
+
+    //get user data
     let email = document.getElementById('input-email').value;
     let password = document.getElementById('input-password').value;
 
-    //get credentials from an API
-    //validate
-    //redirect to dashboard or show an error
 
+    // console.log(email);
+    // console.log(password);
+
+    validateUserInput(email, password);
     
-})
+});
+
+// Function to fetch and validate user input
+async function validateUserInput(email, password) {
+
+    try {
+        // Fetch the JSON file containing user information
+        let response = await fetch('db/users.json');
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch user data');
+        }
+
+        // Parse the JSON data
+        let jsonData = await response.json();
+
+        // Extract the array of users
+        let userData = jsonData.users;
+
+      //console.log(userData);
+  
+      // Validate user input
+      let user = userData.find(user => user.email === email && user.password === password);
+
+      if (user) {
+
+        // Save user profile in localStorage
+        localStorage.setItem('userProfile', JSON.stringify(user));
+
+        document.location.href='dashboard.html';
+        
+      } else {
+        alert('Invalid email or password.');
+        // Handle invalid input (e.g., display error message)
+      }
+    } catch (error) {
+      alert('Error: '+error);
+    }
+  }
